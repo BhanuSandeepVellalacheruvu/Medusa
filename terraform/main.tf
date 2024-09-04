4835052d801dad9a97f8551da7df0329deee77ac
Bhanu sandeep <118993028+BhanuSandeepVellalacheruvu@users.noreply.github.com>
2024-09-04 22:53:19 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -41,27 +41,6 @@ resource "aws_security_group" "medusa_sg" {
   }
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecs_task_execution_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
-  ]
-}
-
 resource "aws_ecs_cluster" "medusa_cluster" {
   name = "medusa_cluster"
 }
@@ -72,7 +51,6 @@ resource "aws_ecs_task_definition" "medusa_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions    = <<DEFINITION
 [
   {
